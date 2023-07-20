00fd1fb1e917821b38777d85f1f8867d0a7b88b7
mdabdullahyusha <emonyusha14@gmail.com>
2023-07-20 13:59:00 +0600

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      inter: "Inter",
+      lobster: "Lobster",
+      poppins: "Poppins",
+      manrope: "Manrope",
+    },
+    colors: {
+      primary: "#FF5959",
+      secondary: "#4F9DA6",
+      third: "#FFF4D6",
+      fourth: "#0B0D17",
+      white: "#fff",
+      black: "#000",
+    },
+  },
+  plugins: [],
+};
